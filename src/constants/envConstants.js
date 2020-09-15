64704c900874c9cf03443c9e6c4b6214dc8fe8e2
Elton Ho <ho.elton@gmail.com>
2020-09-14 23:23:52 -0700

    testing env vars

--- src/constants/envConstants.js
+++ src/constants/envConstants.js
@@ -1,8 +1,8 @@
-export const SERVER = (route) => {return process.env.SERVER_URL + route;} 
+export const SERVER = (route) => {return process.env.REACT_APP_SERVER_URL + route;} 
 
 export const LOCAL_SERVER = "http://localhost:4000/";
 
-export const BASE_SERVER = process.env.SERVER_URL;
+export const BASE_SERVER = process.env.REACT_APP_SERVER_URL;
 
 export const BASE_LOCAL_SERVER = "http://localhost:4000";
 
