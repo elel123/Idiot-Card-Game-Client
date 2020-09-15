41f9ccb2cbada88f5cfad2fb454842bc3b6dc544
Elton Ho <ho.elton@gmail.com>
2020-09-14 23:14:40 -0700

    testing env vars

--- src/constants/envConstants.js
+++ src/constants/envConstants.js
@@ -1,8 +1,8 @@
-export const SERVER = (route) => {return "https://eho-card-game-backend.herokuapp.com/" + route;} 
+export const SERVER = (route) => {return process.env.SERVER_URL + route;} 
 
 export const LOCAL_SERVER = "http://localhost:4000/";
 
-export const BASE_SERVER = "https://eho-card-game-backend.herokuapp.com";
+export const BASE_SERVER = process.env.SERVER_URL;
 
 export const BASE_LOCAL_SERVER = "http://localhost:4000";
 
